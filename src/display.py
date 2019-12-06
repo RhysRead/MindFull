7b9cf29cb856428b55c9bb62dd12d70fb86fce1f
RhysRead <30420852+RhysRead@users.noreply.github.com>
2019-12-06 06:45:56 +0000

    Added best and mean time labels
    Also added some pretext and better formatting

--- src/display.py
+++ src/display.py
@@ -20,9 +20,19 @@ class Display(object):
         self.__root.resizable(0, 0)
         self.__root.attributes('-fullscreen', True)
 
+        self.__all_times = []
+
+        self.__time_rounding_value = 1
+
         self.__base_time = 0
         self.__last_time = 0
         self.__displayed_time = 0
+        self.__mean_time = 0
+        self.__best_time = 0
+
+        self.__best_time_pretext = 'Best: '
+        self.__last_time_pretext = 'Previous: '
+        self.__mean_time_pretext = 'Mean: '
 
         self.__active = False
 
@@ -43,11 +53,23 @@ class Display(object):
                                       fg='Grey')
         self.__timer_label.grid(row=1, column=1, sticky=tk.W, padx=100)
 
+        self.__mean_time_label = tk.Label(self.__root,
+                                          text=self.__mean_time_pretext + '0.0',
+                                          font='Helvetica 25 bold',
+                                          fg='Black')
+        self.__mean_time_label.grid(row=2, column=0, sticky=tk.W, padx=100)
+
         self.__last_time_label = tk.Label(self.__root,
-                                          text=0.0,
+                                          text=self.__last_time_pretext + '0.0',
                                           font='Helvetica 25 bold',
                                           fg='Black')
         self.__last_time_label.grid(row=2, column=1, sticky=tk.W, padx=100)
+
+        self.__best_time_label = tk.Label(self.__root,
+                                          text=self.__best_time_pretext + '0.0',
+                                          font='Helvetica 25 bold',
+                                          fg='Black')
+        self.__best_time_label.grid(row=2, column=2, sticky=tk.W, padx=100)
 
         self.__lost_focus_button = tk.Button(self.__root,
                                              text='Lost Focus',
@@ -79,12 +101,28 @@ class Display(object):
         if not self.__active:
             return
         self.__displayed_time = get_seconds_since_base_time(self.__base_time)
-        self.__timer_label.config(text=round(self.__displayed_time, 1))
+        self.__timer_label.config(text=round(self.__displayed_time,
+                                             self.__time_rounding_value))
         self.__root.after(100, self.__update_timer_recurring)
 
     def __set_new_base_time(self):
         self.__last_time = get_seconds_since_base_time(self.__base_time)
-        self.__last_time_label.config(text=round(self.__last_time, 1))
+
+        self.__all_times.append(self.__last_time)
+
+        # Set best time
+        if self.__last_time > self.__best_time:
+            self.__best_time = self.__last_time
+            self.__best_time_label.config(text=self.__best_time_pretext + str(round(self.__best_time,
+                                                                                    self.__time_rounding_value)))
+        # Set mean time
+        self.__mean_time = sum(self.__all_times) / len(self.__all_times)
+        self.__mean_time_label.config(text=self.__mean_time_pretext + str(round(self.__mean_time,
+                                                                                self.__time_rounding_value)))
+        # Set last time
+        self.__last_time_label.config(text=self.__last_time_pretext + str(round(self.__last_time,
+                                                                                self.__time_rounding_value)))
+
         self.__base_time = time()
 
 
